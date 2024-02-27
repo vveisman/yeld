a820c844d975e2e4c820cb1a5ab4c4c881e1bae0
vincent <vincent914a@gmail.com>
2024-02-27 21:12:44 +0100

    verified

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,6 +1,7 @@
 // "use client";
 
 import { getUser } from "@/lib/actions";
+import Image from "next/image";
 import Link from "next/link";
 
 export default async function Home({
@@ -25,18 +26,28 @@ export default async function Home({
         </div>
         <div className='flex items-center space-x-5'>
           <label htmlFor='address'>Status:</label>
-
-          <p className='texxt-xl lg:text-3xl'>Not Verified</p>
+          <p className='texxt-xl relative lg:text-3xl'>
+            {user?.verified ? "Verified" : "Not Verified"}
+            <Image
+              src={"/verified.webp"}
+              width={15}
+              height={15}
+              alt='verified'
+              className='absolute text-red-500 top-1 -right-5'
+            />
+          </p>
         </div>
 
-        <div className='flex flex-col space-y-5 justify-center items-center'>
-          <p className='w-max '>Click to verify</p>
-          <Link href={"/verify"} className=''>
-            <button className='active:scale-90 lg:-top-2 w-[100px] hover:bg-opacity-70 duration-200  bg-[#283618] text-gray-50 py-2 font-bold rounded-md'>
-              Verify
-            </button>
-          </Link>
-        </div>
+        {!user.verified && (
+          <div className='flex flex-col space-y-5 justify-center items-center'>
+            <p className='w-max '>Click to verify</p>
+            <Link href={"/verify"} className=''>
+              <button className='active:scale-90 lg:-top-2 w-[100px] hover:bg-opacity-70 duration-200  bg-[#283618] text-gray-50 py-2 font-bold rounded-md'>
+                Verify
+              </button>
+            </Link>
+          </div>
+        )}
         <Link href={"/change"} className='self-center'>
           <p className='text-[#33323eff] text-xs self-center underline'>
             Change Password?
